24d1c9177f1ef28cfe6887a83536848792ca24f7
elmorek <elmorek@gmail.com>
2018-08-17 00:39:42 +0200

    Created XSD schemas to validate both requests and responses.

--- schema/schema.py
+++ schema/schema.py
@@ -1,2 +1,19 @@
 import xmlschema
+import sys
 
+
+def validate(xmlfile=None, type='request'):
+    """
+
+    :type xmlfile: str
+    """
+    if type not in ['request', 'response', 'cixml']:
+        print('Invalid xml document type')
+        sys.exit()
+    schema = xmlschema.XMLSchema('{}.xsd'.format(type))
+    if schema.is_valid(xmlfile):
+        try:
+            schema.validate(xmlfile)
+            return schema.is_valid(xmlfile)
+        except xmlschema.XMLSchemaValidationError as e:
+            raise e
