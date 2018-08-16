4227a92a91a5523b4798870a0ec0137a5308b55c
elmorek <elmorek@gmail.com>
2018-08-17 00:37:36 +0200

    Created standard logger.

--- config/config.py
+++ config/config.py
@@ -1,2 +1,12 @@
+import logging
 
 
+def logger(default_level='INFO', file='logs.log'):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    fh = logging.FileHandler(file)
+    fh.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    return logger
