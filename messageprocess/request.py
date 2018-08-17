b8370f6eacc158292a70c344f30dbce2ed1ed8ad
morek <jesus.rodriguez.estevez@gmail.com>
2018-08-17 06:08:27 +0200

    Fixed attribute names

--- messageprocess/request.py
+++ messageprocess/request.py
@@ -1,13 +1,13 @@
 def todict(xml=None):
     assert isinstance(xml, object)
     response = {
-        'role-id' : xml[0][0][0],
+        'roleid' : xml[0][0][0],
         'callingnumber' : xml[0][1][0],
         'callednumber' : xml[0][2][0],
         'transformedcgpn' : xml[0][3][0],
         'transformedcdpn' : xml[0][4][0],
-        'resource-id' : xml[1][0][0],
-        'action-id' : xml[2][0][0],
-        'trigerpointtype' : xml[3][0][0],
+        'resourceid' : xml[1][0][0],
+        'actionid' : xml[2][0][0],
+        'triggerpointtype' : xml[3][0][0],
     }
     return response
